aa07775c41c3ce0683e00596da0cdcff0fd7e274
Medine <mhedekoglu20@gmail.com>
2023-08-03 11:22:55 +0300

    Turtle_Square.py revision

---
+++ Turtle_Square.py
@@ -0,0 +1,11 @@
+import turtle
+
+drawing_board = turtle.Screen()
+drawing_board.bgcolor("pink")
+drawing_board.title("Python Turtle")
+
+turtle_instance = turtle.Turtle()
+
+for i in range(4):
+    turtle_instance.forward(180)
+    turtle_instance.left(90)
